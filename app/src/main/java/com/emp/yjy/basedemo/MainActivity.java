4d1a8ea988b9feec9cfabb69dc276d458a8532cc
linruihang <linruihang@xiongdi.cn>
2020-11-25 17:10:51 +0800

    增加扫描控件示例demo

--- app/src/main/java/com/emp/yjy/basedemo/MainActivity.java
+++ app/src/main/java/com/emp/yjy/basedemo/MainActivity.java
@@ -2,13 +2,59 @@ package com.emp.yjy.basedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.emp.yjy.baselib.base.BaseActivity;
+
+/**
+ * 使用示例
+ *
+ * @author SZ02204
+ */
+public class MainActivity extends BaseActivity implements View.OnClickListener {
+    private Button mBtnScanViewDemo;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int layoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initListener() {
+        super.initListener();
+        mBtnScanViewDemo.setOnClickListener(this::onClick);
+
+    }
+
+    @Override
+    protected void initView() {
+        mBtnScanViewDemo = findViewById(R.id.btn_scan_bar_view_demo);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_scan_bar_view_demo:
+                jumpToActivity(ScanBarViewDemoActivity.class);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private void jumpToActivity(Class<? extends Activity> activityClass) {
+        Intent intent = new Intent(this,activityClass);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/emp/yjy/basedemo/ScanBarViewDemoActivity.java
@@ -0,0 +1,75 @@
+package com.emp.yjy.basedemo;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.emp.yjy.baselib.base.BaseActivity;
+import com.emp.yjy.baselib.utils.DensityUtils;
+import com.emp.yjy.baselib.utils.ScreenUtils;
+import com.emp.yjy.uilib.scan.ScannerBarView;
+
+/**
+ * 使用示例
+ *
+ * @author SZ02204
+ */
+public class ScanBarViewDemoActivity extends BaseActivity implements View.OnClickListener {
+    private ScannerBarView mScannerBarView;
+//    private ScannerBarView2 mScannerBarView;
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected int layoutId() {
+        return R.layout.activity_scanbar_view_demo;
+    }
+
+    @Override
+    protected void initListener() {
+
+    }
+
+    @Override
+    protected void initView() {
+        mScannerBarView = findViewById(R.id.scan_bar_view);
+
+        int appViewWidth = ScreenUtils.getAppViewWidth(this);
+        ViewGroup.LayoutParams layoutParams = mScannerBarView.getLayoutParams();
+        layoutParams.width = (int) (appViewWidth * 0.8);
+        layoutParams.height = (int) (appViewWidth * 0.8);
+        mScannerBarView.setLayoutParams(layoutParams);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mScannerBarView != null) {
+            mScannerBarView.start();
+        }
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mScannerBarView != null) {
+            mScannerBarView.pause();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mScannerBarView != null) {
+            mScannerBarView.stop();
+        }
+    }
+}
